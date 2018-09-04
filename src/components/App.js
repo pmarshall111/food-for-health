9e2f761b7558143d24445955b5f862bed625c3a8
Peter Marshall <pmarshall1993@hotmail.com>
2018-09-04 22:09:14 +0100

    added switcher

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 
 import UserVitals from "./UserVitals";
 
+import Switcher from "./Switcher";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserVitals />
+        <Switcher
+          options={[
+            {
+              desc: "Sign up",
+              onClick: () => {}
+            },
+            {
+              desc: "Log in",
+              onClick: () => {}
+            }
+          ]}
+        />
       </div>
     );
   }
---
+++ src/components/Switcher.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+import "../css/Switcher.css";
+
+class Switcher extends Component {
+  changeBackground(e) {
+    //get the option index, then just translate the background div over to there
+
+    let index = Array.from(e.target.classList)
+      .find(x => x.includes("div&"))
+      .match(/div&(\d+)/)[1];
+
+    // console.log(index);
+
+    const background = document.querySelector(
+      ".switcher-container .background"
+    );
+
+    background.style.left = this.biggestWidth * index + "px";
+  }
+
+  componentDidMount() {
+    const allOptions = document.querySelectorAll(".option-divs");
+    let biggestWidth = -Infinity;
+
+    allOptions.forEach(option => {
+      if (option.clientWidth > biggestWidth) biggestWidth = option.clientWidth;
+    });
+    allOptions.forEach(
+      option => (option.style.width = `${biggestWidth + 1}px`)
+    );
+
+    const background = document.querySelector(
+      ".switcher-container .background"
+    );
+    background.style.width = `${biggestWidth + 1}px`;
+
+    this.biggestWidth = biggestWidth;
+  }
+
+  render() {
+    let { options, name } = this.props;
+
+    let optionDivs = options.map((x, idx) => {
+      return (
+        <div
+          key={`${x}${idx} switcher`}
+          onClick={e => {
+            x.onClick(e);
+            this.changeBackground(e);
+          }}
+          className={`option-divs div&${idx}`}
+        >
+          {x.desc}
+        </div>
+      );
+    });
+
+    return (
+      <div className="switcher-container">
+        <div className="background" />
+        {optionDivs}
+      </div>
+    );
+  }
+}
+
+export default Switcher;
